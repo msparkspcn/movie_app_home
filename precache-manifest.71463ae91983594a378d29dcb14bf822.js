2fa98276b148ad6112e008710fd52bd14191f3d2
minsu <wodfth@gmail.com>
2020-06-07 19:46:10 +0900

    Updates

---
+++ precache-manifest.71463ae91983594a378d29dcb14bf822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b056f6f177a62ae62fcb25a75ce7060",
+    "url": "/movie_app_home/index.html"
+  },
+  {
+    "revision": "18832eb2c34a776381d1",
+    "url": "/movie_app_home/static/css/main.58af61b4.chunk.css"
+  },
+  {
+    "revision": "bca0d5ba092b0a2b5a6f",
+    "url": "/movie_app_home/static/js/2.fdbbb4f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_home/static/js/2.fdbbb4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18832eb2c34a776381d1",
+    "url": "/movie_app_home/static/js/main.db00c371.chunk.js"
+  },
+  {
+    "revision": "b3bc1da9e1c62de23895",
+    "url": "/movie_app_home/static/js/runtime-main.ada35318.js"
+  }
+]);
